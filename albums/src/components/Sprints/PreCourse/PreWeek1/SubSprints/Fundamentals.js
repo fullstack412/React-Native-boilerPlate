f90a489ed5cc63b1718eccb8b3f0357f49d3406c
fullstack412 <dt897867@gmail.com>
2018-12-29 19:46:01 +0200

    Delete extraneous code and edit fundamentals mini sprint title

--- albums/src/components/Sprints/PreCourse/PreWeek1/SubSprints/Fundamentals.js
+++ albums/src/components/Sprints/PreCourse/PreWeek1/SubSprints/Fundamentals.js
@@ -66,12 +66,9 @@ class Fundamentals extends Component {
   }
 
   render() {
-    const { thumbnailStyle, headerConetentStyle, thumbnailContainerStyle, headerTextStyle } = Styles;
-    const topic = 'Fundamentals, Functions, and Booleans'
+    const { headerConetentStyle, headerTextStyle } = Styles;
+    const topic = 'Fundamentals and Functions'
     const introduction = 'This section is dedicated to reviewing JavaScript fundamentals';
-    // const body = 'For limited customization we can use DrawerItems component provided by react-navigation in contentComponent. In case we want to update the styles of the drawer, make it scrollable or add a header/footer we can still achieve it by using the DrawerItems. It will handle the navigation on its own. Below is an example of how to achieve it.';
-    const end = 'This guide covers the various navigation components available in React Native. If you are just getting started with navigation, you will probably want to use React Navigation. React Navigation provides an easy to use navigation solution, with the ability to present common stack navigation and tabbed navigation patterns on both iOS and Android.'
-    console.log('main: ', this.props.navigation)
     return (
       <Card>
         <CardSection>
