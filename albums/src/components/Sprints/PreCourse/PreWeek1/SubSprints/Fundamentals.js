49e8397efc927017aeacf622330bc8373f16f02a
fullstack412 <dt897867@gmail.com>
2018-12-29 19:12:44 +0200

    Write review material for fundamentals mini sprint

--- albums/src/components/Sprints/PreCourse/PreWeek1/SubSprints/Fundamentals.js
+++ albums/src/components/Sprints/PreCourse/PreWeek1/SubSprints/Fundamentals.js
@@ -10,8 +10,8 @@ class Fundamentals extends Component {
   render() {
     const { thumbnailStyle, headerConetentStyle, thumbnailContainerStyle, headerTextStyle } = Styles;
     const topic = 'Fundamentals, Functions, and Booleans'
-    const introduction = 'THIS IS DIFFERENT';
-    const body = 'For limited customization we can use DrawerItems component provided by react-navigation in contentComponent. In case we want to update the styles of the drawer, make it scrollable or add a header/footer we can still achieve it by using the DrawerItems. It will handle the navigation on its own. Below is an example of how to achieve it.';
+    const introduction = 'This section is dedicated to reviewing JavaScript fundamentals';
+    // const body = 'For limited customization we can use DrawerItems component provided by react-navigation in contentComponent. In case we want to update the styles of the drawer, make it scrollable or add a header/footer we can still achieve it by using the DrawerItems. It will handle the navigation on its own. Below is an example of how to achieve it.';
     const end = 'This guide covers the various navigation components available in React Native. If you are just getting started with navigation, you will probably want to use React Navigation. React Navigation provides an easy to use navigation solution, with the ability to present common stack navigation and tabbed navigation patterns on both iOS and Android.'
     console.log('main: ', this.props.navigation)
     return (
@@ -32,13 +32,40 @@ class Fundamentals extends Component {
 
         <CardSection>
           <Text>
-            Body:{"\n"}{body}{"\n"}
+            There are 6 different primitive data types in JavaScript.
+              {"\n"}
+            A primitive is data that is not an object and has no methods
+              {"\n"}
+              Number: 1, 2, 3
+              {"\n"}
+              String: "Hello World"
+              {"\n"}
+              Boolean: true, false
+              {"\n"}
+              Null: var helloWorld = null;
+              {"\n"}
+              Undefined: var goodbyeWorld = undefined;
+              {"\n"}
+              Symbol: var symbol = Symbol();
+              {"\n"}
           </Text>
         </CardSection>
 
         <CardSection>
           <Text>
-            Ending:{"\n"}{end}{"\n"}
+            There are two ways of writing functions.
+            {"\n"}
+            Function declaration: 
+            {"\n"}
+            {`function helloWorld() {
+              return "hello world"
+            }`}
+            {"\n"}
+            Function expression: 
+            {"\n"}
+            {`var helloWorld = function() {
+              return "hello world"
+            }`}
           </Text>
         </CardSection>
         
