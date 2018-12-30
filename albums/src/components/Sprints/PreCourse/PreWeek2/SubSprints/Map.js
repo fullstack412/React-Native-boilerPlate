79c90fe301ca1804143609d585c35a47f4c5bc74
fullstack412 <dt897867@gmail.com>
2018-12-30 17:52:21 +0200

    git files ready for pre-course /week 2

---
+++ albums/src/components/Sprints/PreCourse/PreWeek2/SubSprints/Map.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Card, CardSection, Playquiz } from '../../../../common';
+
+
+class Map extends Component{
+  constructor(props){
+      super(props)
+      this.state{
+
+      }
+  }
+  render(){
+      return(
+
+      )
+  }
+}
+
+const Styles=StyleSheet.create({
+    
+})
+
+export default Map
