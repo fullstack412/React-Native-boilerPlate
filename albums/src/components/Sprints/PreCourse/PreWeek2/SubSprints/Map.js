82af2c2e0562deb99176a2ad5e2a4a2b302c2713
fullstack412 <dt897867@gmail.com>
2019-01-02 20:22:12 +0200

    text added to Map preCourse

--- albums/src/components/Sprints/PreCourse/PreWeek2/SubSprints/Map.js
+++ albums/src/components/Sprints/PreCourse/PreWeek2/SubSprints/Map.js
@@ -10,12 +10,48 @@ class Map extends Component{
      
   }
   render(){
+    const topic='Map';
+    const Difine='The map() method is used to apply a function on every element in an array.\n A new array is then returned.';
+    const function1='function map(n,f){\n var acc=[]\n each(n,function(element,i){\n acc.push(f(element,i))\n })\n return acc \n };';
+    const ImplementEach='Of Course, you have to declare Each() before because you will get (Each is not defined) ';
+    const ImproveMap='function map(n,f){\n var acc=[]\n if(!Array.isArray(n)){\n acc={}\n  }\n each(n,function(element,key){\n acc[key]=f(element,key })\n return acc\n  }'
     const { thumbnailStyle,title,container,textContainer,thumbnailStyle1}=Styles
     return(
       <Card>
         <CardSection>
-
+        <View>
+            <Text style={title}>
+              {topic}{"\n"}
+            </Text>
+          </View>
         </CardSection>
+        <CardSection style={container}>
+                <Image 
+                    style= {thumbnailStyle}
+                    source={require('../../../../../assets/Pic5.jpeg')}/>
+                
+                <Text style={textContainer} >
+                    {Difine} {"\n"}{"\n"}
+                </Text>
+            </CardSection >
+            <CardSection style={container}> 
+              <Text style={{fontWeight:'bold',color:'green'}}>
+                {function1}{"\n"}{"\n"}
+              </Text>
+              <Image
+              style={{width:300,height:175}}
+              source={require('../../../../../assets/Pic6.png')}/>
+            </CardSection>
+            <CardSection>
+              <Text>
+                {ImplementEach}{"\n"}{"\n"}
+                To make our function works with objects also:{'\n'}{"\n"}
+                <Text style={{fontWeight:'bold',color:'green'}}>
+                {ImproveMap}
+
+                </Text>
+              </Text>
+            </CardSection>
       </Card>
     )
      
@@ -35,13 +71,7 @@ fontSize: 19,
 fontWeight: 'bold'
 
 },
-thumbnailStyle: {
-    height: 150 ,
-    width: 350
-  
-    
-  },
-  thumbnailStyle1:{
+thumbnailStyle:{
     height: 100 ,
     width: 100
   }
