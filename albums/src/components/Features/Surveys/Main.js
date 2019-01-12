a74a758b140d7d649c1633f2edc24b87ec9151b1
fullstack412 <dt897867@gmail.com>
2019-01-12 22:44:01 +0200

    fixing multiple choice

--- albums/src/components/Features/Surveys/Main.js
+++ albums/src/components/Features/Surveys/Main.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, TextInput, StyleSheet, TouchableOpacity, Text, View, Picker } from 'react-native';
 import { Header, Button, Card, CardSection } from '../../common';
+import firebase from 'firebase';
+import api from '../../../../';
+import MultipleChoice from 'react-native-multiple-choice';
 
 class Main extends Component {
 
   
   render() {
   
-    const { thumbnailStyle, headerConetentStyle, thumbnailContainerStyle, headerTextStyle } = Styles;
+    const { thumbnailStyle, headerConetentStyle, thumbnailContainerStyle, headerTextStyle, container } = Styles;
     const topic = 'Emotional Survey'
     const introduction = 'Your emtional health is very important to us';
     
@@ -24,11 +27,27 @@ class Main extends Component {
 
         <CardSection>
           <Text>
-            Input:{"\n"}{introduction}{"\n"}
+            {introduction}
           </Text>
+          <View style={container}>
+            <MultipleChoice
+              options={[
+                'Super Happy',
+                'Happy',
+                'Doing Okay',
+                'Upset',
+                'Sad'
+              ]}
+              selectedOptions={['Doing Okay']}
+              maxSelectedOptions={1}
+              onSelection={(option)=>{
+                Alert.alert(option + ' was selected!')
+              }}
+            />
+          </View>
         </CardSection>
 
-                  <CardSection>
+          <CardSection>
             <Button onPress = {()=>this.props.navigation.navigate('Calendar')}>
               Calendar
             </Button>
@@ -67,6 +86,10 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: 20,
     marginRight: 20
+  },
+  container: {
+    marginTop: 60,
+    margin: 20
   }
 });
 export default Main;
