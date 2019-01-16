1d3bc93850ab2c5f84f055569eca684fc70fba1b
fullstack412 <dt897867@gmail.com>
2019-01-16 10:54:27 +0200

    edit OOP  style

--- albums/src/components/Sprints/PreCourse/PreWeek3/SubSprints/OOP.js
+++ albums/src/components/Sprints/PreCourse/PreWeek3/SubSprints/OOP.js
@@ -9,7 +9,7 @@ export default class OOP extends Component {
   render() {
     const topic = 'OOP';
     const introduction = 'OOP (Object-Oriented Programming) is an approach in programming in which data is encapsulated within objects and the object itself is operated on, rather than its component parts.';
-    const { headerConetentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle, boldText, boldHeader, text, variable} = Styles
+    const { headerConetentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle, boldText, boldHeader, text, variable } = Styles
     return (
       <Card>
         <CardSection>
@@ -40,7 +40,7 @@ export default class OOP extends Component {
               use a lot of space in a big scale
             </Text>
             , so lets think in a way to decrease this space.{'\n'}
-  
+
             1- Put the functions outside to have it one time no matter how much we make copies from the main function.{'\n'}
             2- Reach this functions by pass thier name inside the key in the object that we returnd.{'\n'}
             <Text style={boldText}>
@@ -63,7 +63,7 @@ export default class OOP extends Component {
             </Text>
           </Text>
         </CardSection  >
-  
+
         <CardSection  >
           <Text style={boldText}>
             Let's take an example and break it down:
@@ -71,33 +71,8 @@ export default class OOP extends Component {
         </CardSection>
 
         <OOPExample />
+        
         <OOPExampleExplanation />
-
-        <CardSection  >
-          <Text style={boldText}>
-            Now we will develop the function that we did
-            before we have problem about each time we
-            make a new variable from the function it
-             will take another copy from it and this
-             thing will use a lot of space in big scale
-            so lets think in a way to decrease this
-             space make the function outside and to
-             reach them by the object we but them in the
-             key of tis object but now how we can pass
-             the value from this object to the function
-             are outside so here where we will use the
-             keyword “this” what is this? It is a
-             keyword represent the object in the left
-              of the dot (the one who called this function)
-              [90% the time]
-          </Text>
-        </CardSection>
-
-        {/* <CardSection >
-          <Image
-            style={{ width: 340, height: 125 }}
-            source={require('../assets/reduceWithIV.png')} />
-        </CardSection> */}
 
         <CardSection>
           <Button onPress={() => this.props.navigation.navigate('Closures And Adding Methods')}>
--- albums/src/components/Sprints/PreCourse/PreWeek3/SubSubSprints/OOPExampleExplanation.js
+++ albums/src/components/Sprints/PreCourse/PreWeek3/SubSubSprints/OOPExampleExplanation.js
@@ -8,35 +8,10 @@ export default class OOPExampleExplanation extends Component {
     return (
       <CardSection>
         <Text style={text}>
-          Now we will improve the function that we takes before.{'\n'}
-          We said that we have a problem, so what is this problem?{'\n'}
-          The problem is each time we make a new variable from the function
-          it will take another copy from it and this problem will{' '}
-          <Text style={boldText}>
-            use a lot of space in a big scale
-          </Text>
-          , so lets think in a way to decrease this space.{'\n'}
+          <Text style={text}>
 
-          1- Put the functions outside to have it one time no matter how much we make copies from the main function.{'\n'}
-          2- Reach this functions by pass thier name inside the key in the object that we returnd.{'\n'}
-          <Text style={boldText}>
-            <Text style={variable}>
-              But we have a problem, can you guess what is it?{'\n'}
-            </Text>
-            It is about how we can pass the value from this object to the functions are outside.{'\n'}
           </Text>
-          So to solve this problem we will use the keyword{' '}
-          <Text style={boldText}>
-            “this”
-          </Text>
-          ,what is the keyword{' '}
-          <Text style={boldText}>
-            “this”{' '}
-          </Text>
-          ?{'\n'}
-          <Text style={[variable, boldText]}>
-            [90% of the time] It represents the object in the left of the dot (the one who called this function).{'\n'}{'\n'}
-          </Text>
+
         </Text>
       </CardSection >
     );
